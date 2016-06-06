6f4bcd9ae0faa78408989751b185f8d9031e727c
Sharath Rao <sharathrao13@gmail.com>
2016-06-06 10:42:16 -0700

    Remove scripts from root folder

--- log1.sh
+++
@@ -1,3 +0,0 @@
-less +F /home/sharath/Raft/log/raft_node.log
-less +F /home/sharath/Raft2/log/raft_node.log
-less +F /home/sharath/Raft3/log/raft_node.log
--- start_script.sh
+++
@@ -1,3 +0,0 @@
-python /home/sharath/Raft/server/raft_node.py &;
-python /home/sharath/Raft2/server/raft_node.py &;
-python /home/sharath/Raft3/server/raft_node.py &;
