0af26c2bb9770af9cd82ee3bc3d8d8637f272a31
Suraj Rajesh <surajx.rajesh@gmail.com>
2016-06-06 19:01:02 -0700

    Script fix

--- scripts/clean_start_template.sh
+++ scripts/clean_start_template.sh
@@ -2,4 +2,5 @@ rm ../config/config.ini
 cp ../config/config0.ini ../config/config.ini
 rm ../persistence/*.p
 python ../server/raft_node.py &
+sleep 2
 tail -f ../log/raft_node.log
--- scripts/start.sh
+++ scripts/start.sh
@@ -1,2 +1,3 @@
 python ../server/raft_node.py &
+sleep 2
 tail -f ../log/raft_node.log
