769d53b9b9aa46efee0ad409f878f003c56a02b3
Suraj Rajesh <surajx.rajesh@gmail.com>
2016-06-06 18:39:29 -0700

    Startup scripts

---
+++ scripts/clean_start_template.sh
@@ -0,0 +1,5 @@
+rm ../config/config.ini
+cp ../config/config0.ini ../config/config.ini
+rm ../persistence/*.p
+python ../server/raft_node.py &
+tail -f ../log/raft_node.log
---
+++ scripts/start.sh
@@ -0,0 +1,2 @@
+python ../server/raft_node.py &
+tail -f ../log/raft_node.log
